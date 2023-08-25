ad356ac3ad89a6323beb672cea0a3e4e34f97fa8
Luca Bruno <lucabru@gmail.com>
2023-08-26 00:40:24 +0200

    fix: format

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,11 +9,7 @@ export const metadata: Metadata = {
   description: "Portfolio for a passionate full stack software developer 👨‍💻. Based in Malta 🇲🇹."
 }
 
-export default function RootLayout({
-  children
-}: {
-  children: React.ReactNode
-}) {
+export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
       <body className={inter.className}>

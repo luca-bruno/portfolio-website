c747b7b607ba2a6b8beca1a9460bc13c3c44f20d
Luca Bruno <lucabru@gmail.com>
2023-09-03 12:50:41 +0200

    fix: SlideShowPreview as separate component + slide show CSS fix & cleanup [95% wip]

--- components/SlideShow/SlideShow.tsx
+++ components/SlideShow/SlideShow.tsx
@@ -1,82 +1,22 @@
-import { faCircle, faUpRightAndDownLeftFromCenter, faChevronRight, faX, faChevronLeft } from "@fortawesome/free-solid-svg-icons"
+import { faCircle, faX } from "@fortawesome/free-solid-svg-icons"
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
 import React, { useState } from "react"
 import "@/styles/SlideShow.css"
-import Image from "next/image"
-import { trivya1mobile } from "@/assets"
 import { SlideShowTypes } from "./types/SlideShow.interface"
 import SlideShowViewToggle from "./SlideShowViewToggle"
-import { defaultImage } from "../Cards/data"
+import SlideShowPreview from "./SlideShowPreview"
 
 const SlideShow: React.FC<SlideShowTypes> = ({ images }) => {
   const [currentSlide, setCurrentSlide] = useState(0)
   const [fullscreenMode, setFullscreenMode] = useState(false)
   const [isMobileView, setIsMobileView] = useState(false)
-  const [isArrowHovered, setIsArrowHovered] = useState(false)
 
   const hasMoreThanOneImage = images && images.length <= 1
-  const isOnFirstPage = currentSlide === 0
-  const isOnLastPage = currentSlide === (images && images.length - 1)
   const isOnCurrentSlide = (slide: number) => slide === currentSlide
 
   return (
     <>
-      <div className={`sidepanel_slideshow ${isMobileView ? "mobile" : ""}`}>
-        {/* TODO: <SlideShowPreview /> */}
-        {!isMobileView ?
-          <Image
-            draggable="false"
-            alt="card"
-            width={1920}
-            height={1080}
-            className={`sidepanel_slideshow ${currentSlide ? "active" : ""} ${isArrowHovered ? "slide_no_hover" : "slide_hover"}`}
-            src={images?.[currentSlide] || defaultImage} />
-          : <Image draggable="false" alt="card" width={425} height={844} src={trivya1mobile} />
-        }
-
-        {/* TODO: <SlideShowPreviewButtons (?) /> */}
-        <button
-          type="button"
-          className={`overlay overlay_left ${isArrowHovered ? "overlay_hovered" : ""}`}
-          disabled={isOnFirstPage}
-          onMouseEnter={() => setIsArrowHovered(true)}
-          onMouseLeave={() => setIsArrowHovered(false)}
-          onClick={() => setCurrentSlide(prev => prev - 1)}
-        >
-          <div className="overlay-content">
-            <div className={`icon icon_arrow ${isOnFirstPage ? "overlay_disabled" : ""}`}>
-              <FontAwesomeIcon icon={faChevronLeft} />
-            </div>
-          </div>
-        </button>
-
-        <button
-          type="button"
-          className={`${isArrowHovered ? "overlay_hidden" : "overlay overlay_center"}`}
-          onClick={() => setFullscreenMode(true)}
-        >
-          <div className={`${isArrowHovered ? "overlay_hidden" : "overlay-content"}`}>
-            <div className="icon icon_expand">
-              <FontAwesomeIcon icon={faUpRightAndDownLeftFromCenter} />
-            </div>
-          </div>
-        </button>
-
-        <button
-          type="button"
-          className={`overlay overlay_right ${isArrowHovered ? "overlay_hovered" : ""}`}
-          disabled={isOnLastPage}
-          onMouseEnter={() => setIsArrowHovered(true)}
-          onMouseLeave={() => setIsArrowHovered(false)}
-          onClick={() => setCurrentSlide(prev => prev + 1)}>
-          <div className="overlay-content">
-            <div className={`icon icon_arrow ${isOnLastPage ? "overlay_disabled" : ""}`}>
-              <FontAwesomeIcon icon={faChevronRight} />
-            </div>
-          </div>
-        </button>
-
-      </div>
+      <SlideShowPreview {...{ currentSlide, setCurrentSlide, setFullscreenMode, isMobileView, images }} />
 
       <div>
         {/* TODO: <SlideShowPageDots (?) /> */}
---
+++ components/SlideShow/SlideShowPreview.tsx
@@ -0,0 +1,85 @@
+import React, { useState } from "react"
+import Image from "next/image"
+import { trivya1mobile } from "@/assets"
+import "@/styles/SlideShowPreview.css"
+import { faUpRightAndDownLeftFromCenter, faChevronLeft, faChevronRight } from "@fortawesome/free-solid-svg-icons"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import { defaultImage } from "../Cards/data"
+
+const SlideShowPreview = ({ currentSlide, setCurrentSlide, setFullscreenMode, isMobileView, images }) => {
+    const [isArrowHovered, setIsArrowHovered] = useState(false)
+
+    const isOnFirstPage = currentSlide === 0
+    const isOnLastPage = currentSlide === (images && images.length - 1)
+
+    return (
+        <div className={`slide_show_container ${isMobileView ? "mobile" : ""}`}>
+            {/* TODO: <SlideShowPreview /> */}
+            {!isMobileView ?
+                <Image
+                    draggable="false"
+                    alt="card"
+                    width={1920}
+                    height={1080}
+                    className={`slide_show_container ${currentSlide ? "active" : ""} ${isArrowHovered ? "slide_no_hover" : "slide_hover"}`}
+                    src={images?.[currentSlide] || defaultImage} />
+                :
+                <Image draggable="false" alt="card" width={425} height={844} src={trivya1mobile} />
+            }
+
+            {/* TODO: <SlideShowPreviewButtons (?) /> */}
+
+
+            <div className={`${isArrowHovered ? "overlay_center_container2" : "overlay_center_container"}`}>
+                <button
+                    type="button"
+                    className="overlay_center"
+                    onClick={() => setFullscreenMode(true)}
+                >
+                    <div>
+                        <div className="icon icon_expand">
+                            <FontAwesomeIcon icon={faUpRightAndDownLeftFromCenter} />
+                        </div>
+                    </div>
+                </button>
+            </div>
+
+            <div className="">
+                <button
+                    type="button"
+                    className={`overlay_left_container overlay overlay_left ${isArrowHovered ? "overlay_hovered" : ""}`}
+                    disabled={isOnFirstPage}
+                    onMouseEnter={() => setIsArrowHovered(true)}
+                    onMouseLeave={() => setIsArrowHovered(false)}
+                    onClick={() => setCurrentSlide(prev => prev - 1)}
+                >
+                    <div>
+                        <div className={`icon icon_arrow ${isOnFirstPage ? "overlay_disabled" : ""}`}>
+                            <FontAwesomeIcon icon={faChevronLeft} />
+                        </div>
+                    </div>
+                </button>
+            </div>
+
+            <div className="">
+                <button
+                    type="button"
+                    className={`overlay_right_container overlay overlay_right ${isArrowHovered ? "overlay_hovered" : ""}`}
+                    disabled={isOnLastPage}
+                    onMouseEnter={() => setIsArrowHovered(true)}
+                    onMouseLeave={() => setIsArrowHovered(false)}
+                    onClick={() => setCurrentSlide(prev => prev + 1)}>
+                    <div>
+                        <div className={`icon icon_arrow ${isOnLastPage ? "overlay_disabled" : ""}`}>
+                            <FontAwesomeIcon icon={faChevronRight} />
+                        </div>
+                    </div>
+                </button>
+            </div>
+
+        </div>
+    )
+}
+
+
+export default SlideShowPreview

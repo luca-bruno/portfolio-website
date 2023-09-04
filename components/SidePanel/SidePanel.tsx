ee026f0d7414d21a4a2ce24a3dc357a3a522a080
Luca Bruno <lucabru@gmail.com>
2023-09-04 23:59:20 +0200

    fix: moved SidePanel close button drawer + styling

--- components/SidePanel/SidePanel.tsx
+++ components/SidePanel/SidePanel.tsx
@@ -26,84 +26,90 @@ const SidePanel: React.FC<SidePanelTypes> = ({
 
   return (
     // TODO: split components
-    <div className={`sidepanel ${isSidePanelOpen ? "open" : "closed"}`}>
-      <button
-        type="button"
-        className="sidepanel_close_button"
-        onClick={() => {
-          setIsSidePanelOpen(false)
-          setActiveCard(undefined)
-        }}
-      >
-        <FontAwesomeIcon
-          className="sidepanel_close_button_icon"
-          icon={faChevronRight}
-        />
-      </button>
+    <>
+      {isSidePanelOpen &&
+        <div>
+          <button
+            type="button"
+            className="sidepanel_close_button sidepanel_extension"
+            onClick={() => {
+              setIsSidePanelOpen(false)
+              setActiveCard(undefined)
+            }}
+          >
+            <FontAwesomeIcon
+              className="sidepanel_close_button_icon sidepanel_extension_icon"
+              icon={faChevronRight}
+            />
+          </button>
+        </div>
+      }
 
-      {isSidePanelOpen && activeCardDetails && (
-        <div className="sidepanel_content">
-          <SlideShow {...{ images }} />
+      <div className={`sidepanel ${isSidePanelOpen ? "open" : "closed"}`}>
+        {isSidePanelOpen && activeCardDetails && (
+          <div className="sidepanel_content">
+            <SlideShow {...{ images }} />
 
-          <div className="sidepanel_info">
-            <p className="sidepanel_title">{name}</p>
-            <p className="sidepanel_description">
-              Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
-              laboriosam impedit accusamus ex repellendus, ab excepturi?
-              Asperiores, vitae, consequatur dolorum aliquid aliquam est
-              perferendis recusandae eveniet at reiciendis saepe architecto!
-            </p>
-          </div>
-
-          <div className="sidepanel_buttons">
-            {hosted && (
-              <a href={hosted} rel="noreferrer" target="_blank">
-                <button type="button" className="sidepanel_button">
-                  <FontAwesomeIcon
-                    className="sidepanel_button_icon"
-                    icon={faLink}
-                  />
-                  Visit live site
-                </button>
-              </a>
-            )}
-
-            {repo && (
-              <a href={repo} rel="noreferrer" target="_blank">
-                <button type="button" className="sidepanel_button">
-                  <FontAwesomeIcon
-                    className="sidepanel_button_icon"
-                    icon={faGithub}
-                  />
-                  View repository on GitHub
-                </button>
-              </a>
-            )}
-          </div>
-
-          {stack && (
-            <div className="sidepanel_stack">
-              <p className="sidepanel_stack_title">
-                Made using the following technologies:
+            <div className="sidepanel_info">
+              <p className="sidepanel_title">{name}</p>
+              <p className="sidepanel_description">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Facilis
+                laboriosam impedit accusamus ex repellendus, ab excepturi?
+                Asperiores, vitae, consequatur dolorum aliquid aliquam est
+                perferendis recusandae eveniet at reiciendis saepe architecto!
               </p>
-              {stack.map(
-                (tech: string) =>
-                  tech && (
-                    <Image
-                      key={tech}
-                      className="sidepanel_stack_item_icon"
-                      src={check(tech)}
-                      alt="My SVG"
-                      width={50}
-                      height={50}
+            </div>
+
+            <div className="sidepanel_buttons">
+              {hosted && (
+                <a href={hosted} rel="noreferrer" target="_blank">
+                  <button type="button" className="sidepanel_button">
+                    <FontAwesomeIcon
+                      className="sidepanel_button_icon"
+                      icon={faLink}
                     />
-                  )
+                    Visit live site
+                  </button>
+                </a>
+              )}
+
+              {repo && (
+                <a href={repo} rel="noreferrer" target="_blank">
+                  <button type="button" className="sidepanel_button">
+                    <FontAwesomeIcon
+                      className="sidepanel_button_icon"
+                      icon={faGithub}
+                    />
+                    View repository on GitHub
+                  </button>
+                </a>
               )}
             </div>
-          )}
-        </div>
-      )}
-    </div>
+
+            {stack && (
+              <div className="sidepanel_stack">
+                <p className="sidepanel_stack_title">
+                  Made using the following technologies:
+                </p>
+                {stack.map(
+                  (tech: string) =>
+                    tech && (
+                      <Image
+                        key={tech}
+                        className="sidepanel_stack_item_icon"
+                        src={check(tech)}
+                        alt="My SVG"
+                        width={50}
+                        height={50}
+                      />
+                    )
+                )}
+              </div>
+            )}
+          </div>
+        )}
+      </div>
+    </>
   )
 }
 

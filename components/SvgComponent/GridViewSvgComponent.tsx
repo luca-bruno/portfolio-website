8cd3017da52168f29a592d8b13f145bd8f5bab2c
Luca Bruno <lucabru@gmail.com>
2024-04-11 22:19:54 +0200

    feat: Featured, Compact, Grid, List view custom SVG components

---
+++ components/SvgComponent/GridViewSvgComponent.tsx
@@ -0,0 +1,12 @@
+import React from "react"
+
+const GridViewSvgComponent = () => (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="24" height="24">
+        <rect x="5" y="5" width="6" height="6" rx="3" ry="1" />
+        <rect x="13" y="5" width="6" height="6" rx="3" ry="1" />
+        <rect x="5" y="13" width="6" height="6" rx="3" ry="1" />
+        <rect x="13" y="13" width="6" height="6" rx="3" ry="1" />
+    </svg>
+)
+
+export default GridViewSvgComponent
--- components/SvgComponent/index.ts
+++ components/SvgComponent/index.ts
@@ -2,10 +2,18 @@ import BehanceSvgComponent from "./BehanceSvgComponent"
 import BrunoLucaSvgComponent from "./BrunoLucaSvgComponent"
 import GitHubSvgComponent from "./GitHubSvgComponent"
 import LinkedInSvgComponent from "./LinkedInSvgComponent"
+import ListViewSvgComponent from "./ListViewSvgComponent"
+import CompactViewSvgComponent from "./CompactViewSvgComponent"
+import GridViewSvgComponent from "./GridViewSvgComponent"
+import FeaturedViewSvgComponent from "./FeaturedViewSvgComponent"
 
 export {
     BehanceSvgComponent,
     BrunoLucaSvgComponent,
     GitHubSvgComponent,
     LinkedInSvgComponent
+    ListViewSvgComponent,
+    CompactViewSvgComponent,
+    GridViewSvgComponent,
+    FeaturedViewSvgComponent
 }

c5bcb6c1e001b2d019d5a8631db3dc0b7753687f
Luca Bruno <lucabru@gmail.com>
2024-04-11 20:03:07 +0200

    feat: Header Logo + minor cleanup

--- components/Header/Header.tsx
+++ components/Header/Header.tsx
@@ -4,56 +4,60 @@ import { usePathname } from "next/navigation"
 import Link from "next/link"
 import "@/styles/Header.css"
 import { rubik } from "@/app/fonts"
+import BrunoLucaIcon from "@/public/assets/icons/brunoluca-icon.svg"
+import Image from "next/image"
 
 const Header = () => {
   const pathname = usePathname()
 
-  const isActive = (currentPathname: string) =>
-    currentPathname === pathname ? "header_buttons_button_active" : ""
+  const isActive = (currentPathname: string) => currentPathname === pathname ? "header_buttons_button_active" : ""
 
   const isCurrentlyOnHomepage = pathname === "/"
+
+  const leftHandSideStyle = "header_name fade_in"
+  const rightHandSideStyle = "header_buttons_button fade_in"
 
   return (
     <header>
       <nav>
         <div className={`${rubik.className} header`}>
-          {!isCurrentlyOnHomepage ?
+          {isCurrentlyOnHomepage ?
+            <Image
+              width={38}
+              height={38}
+              className={`${leftHandSideStyle} grayscale`}
+              src={BrunoLucaIcon}
+              alt="BrunoLuca icon"
+              draggable="false"
+            />
+            :
             <Link
               href="/"
-              className={`header_name fade_in ${isActive("/")} `}
+              className={`${leftHandSideStyle} ${isActive("/")} `}
               draggable={false}
             >
               LUCA BRUNO
             </Link>
-            :
-            <div />
           }
           <div className="header_buttons">
             <Link
               href="about"
-              className={`header_buttons_button fade_in ${isActive("/about")}`}
+              className={`${rightHandSideStyle} ${isActive("/about")}`}
               draggable={false}
             >
               About
             </Link>
             <Link
               href="projects"
-              className={`header_buttons_button fade_in ${isActive("/projects")}`}
+              className={`${rightHandSideStyle} ${isActive("/projects")}`}
               draggable={false}
             >
               Projects
             </Link>
-            <Link
-              href="contact"
-              className={`header_buttons_button fade_in ${isActive("/contact")}`}
-              draggable={false}
-            >
-              Contact
-            </Link>
           </div>
         </div>
       </nav>
-    </header >
+    </header>
   )
 }
 

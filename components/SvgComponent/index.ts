f683827a65e2cef71a8a5a3618d7ce6438dc569d
Luca Bruno <lucabru@gmail.com>
2024-04-11 20:04:49 +0200

    feat: SVG Components for Contact icons

---
+++ components/SvgComponent/index.ts
@@ -0,0 +1,11 @@
+import BehanceSvgComponent from "./BehanceSvgComponent"
+import BrunoLucaSvgComponent from "./BrunoLucaSvgComponent"
+import GitHubSvgComponent from "./GitHubSvgComponent"
+import LinkedInSvgComponent from "./LinkedInSvgComponent"
+
+export {
+    BehanceSvgComponent,
+    BrunoLucaSvgComponent,
+    GitHubSvgComponent,
+    LinkedInSvgComponent
+}
